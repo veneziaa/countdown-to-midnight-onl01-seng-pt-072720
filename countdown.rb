be5ada19df918c79ed4bf10c4c9c4ed85623e86d
Venice Vargas <venicevargas1@live.com>
2020-08-04 04:24:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 number = 10
-  while number < 10 
-    puts "#{number} SECOND(S)!"
-    number -= 1
-  
+while number < 10 
+  puts "#{number} SECOND(S)!"
+number -= 1
+
   def countdown
     put "HAPPY NEW YEAR!"
 end
